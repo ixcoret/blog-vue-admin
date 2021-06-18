21f39fdeca1e10be17d3614f005b5571548d38b5
ixcoret <song1036752344@163.com>
2021-06-18 18:05:58 +0800

    修改文件

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="dashboard-container">
-    <div class="dashboard-text">首页</div>
+    <div class="dashboard-text">
+      TODO: 展示博客前台访问量、前台用户量、博客点赞量评论量等功能，待开发...
+    </div>
   </div>
 </template>
 
